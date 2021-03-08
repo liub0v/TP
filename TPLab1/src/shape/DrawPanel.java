e78dca3db9d43faac01650dc4b98ea67f3b02eeb
Stacey Korbit <nastyasworld@gmail.com>
2021-03-08 20:22:10 +0300

    regular added

--- TPLab1/src/shape/DrawPanel.java
+++ TPLab1/src/shape/DrawPanel.java
@@ -131,6 +131,11 @@ public class DrawPanel extends Canvas {
                             myShape = new Parallelogram(points.get(pointsNumber - 2), points.get(pointsNumber - 1), borderColor, fillColor);
                             myShape.draw(gc);
                             break;
+                        case "Regular Polygon":
+                            myShape = new RegularPolygon( points.get(pointsNumber - 2), points.get(pointsNumber - 1),
+                                    borderColor, Color.WHITE, 4);
+                            myShape.draw(gc);
+                            break;
                         case "Circle":
                             myShape = new Circle(points.get(pointsNumber - 2), points.get(pointsNumber - 1), borderColor, fillColor);
                             myShape.draw(gc);
